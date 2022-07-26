38a4472b251f68e9007545292bbcbe1db1d76c65
Ashok Kumar Rawat <1510ashokrawat@gmail.com>
2022-07-27 00:31:22 +0530

    header file edited

--- src/component/header_component/HeaderComponent.js
+++ src/component/header_component/HeaderComponent.js
@@ -5,17 +5,10 @@ import { MdMenu } from "react-icons/md";
 import { useState } from "react";
 // useScroll custom hook
 import useScroll from "./useScroll";
-// framer motion for navbar animation
-import { motion } from "framer-motion";
-// framer motion for navbar animation
-const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 1, x: "-20%" },
-};
-// framer motion for navbar animation
+
 function HeaderComponent() {
   const [toggle, setToggle] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+
   // useScroll custom hook
   const { navbar } = useScroll();
 
@@ -27,17 +20,13 @@ function HeaderComponent() {
         </div>
         <MdMenu
           onClick={() => {
-            setIsOpen((isOpen) => !isOpen);
             setToggle(!toggle);
           }}
         />
-        <motion.div
-          animate={isOpen ? "open" : "closed"}
-          variants={variants}
-          className={toggle ? "navbar--links show" : "navbar--links hide"}
-        >
-          <NavComponent onClickFunction={() => setToggle(!toggle)} />
-        </motion.div>
+
+        <div className={toggle ? "navbar--links show" : "navbar--links hide"}>
+          <NavComponent onClickFunction={() => setToggle(false)} />
+        </div>
       </div>
     </div>
   );
